0ee78c5517cbab56367b56d6e54c76f7d6fe5dda
mickey910611 <132209186+mickey910611@users.noreply.github.com>
2023-05-03 15:21:15 +0800

    .

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ else
                 <input id = "log_bt" type="button" value="登入" onclick = "location.href = 'log_in_page.php'">
                 <input id = "sign_bt" type="button" value="註冊" onclick = "location.href = 'sign_up_page.php'">
                 <input id = "manager_bt" type="button" value="管理員" onclick = "location.href = 'manager_login_page.php'">
+                <input id = "mem_info_bt" type="button" value="會員資訊" onclick = "location.href = 'member_info_page.php'">
             <?php
             }
             else
--- member_info_page.php
+++ member_info_page.php
@@ -12,14 +12,13 @@
                 <h2>這裡會有個頭貼</h2>
                 <img style="width:200px"alt="BMW LOGO" id = "head_pic" src = "./pics/BMW_logo.png">             
                 <input id = "index_bt" type="button" value="健身房" onclick = "location.href = 'index.php'">
-                <input id = "mem_info_bt" type="button" value="會員資訊" onclick = "location.href = 'member_info_page.php'">
                 <input id = "logout_bt" type="button" value="登出" onclick = "location.href = 'log_out.php'">
                 <input id = "change_bt" type="button" value="修改密碼" onclick = "location.href = 'change_password.php'">
             </div>
             <div id = "section_1">
                 <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>         
-                <p class = "text_a" id = "cat">喵喵喵</p>
-                <p class = "text_a" id = "cat_text">會員資訊的東東 對。</p>
+                <p class = "text_a" id = "class">已預約xxxx/xx/xx的課程</p>
+                <p class = "text_a" id = "plan">目前正使用...方案</p>
                 <br/><br/><br/>
                 <img style="width:300px"alt="Gurobi最帥" id = "pic_a" src = "./pics/0426.png">
             </div>
