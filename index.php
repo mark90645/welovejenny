ab71dd7c352b82a3d8098ff66d797909a4c95575
mark90645 <132208738+mark90645@users.noreply.github.com>
2023-05-04 15:06:54 +0800

    0504

--- index.php
+++ index.php
@@ -24,24 +24,24 @@ else
             <div class = "banner">
                 <input id = "index_bt" type="button" value="健身房" onclick = "location.href = 'index.php'">
                 <?php
-            if ($log_check == 0)
-            {
+                if ($log_check == 0)
+                {
                 ?>
                 <input class = "bt" id = "log_bt" type="button" value="登入" onclick = "location.href = 'log_in_page.php'">
                 <input class = "bt" id = "sign_bt" type="button" value="註冊" onclick = "location.href = 'sign_up_page.php'">
                 <input class = "bt" id = "manager_bt" type="button" value="管理員登入" onclick = "location.href = 'manager_login_page.php'">
                 <input class = "bt" id = "mem_info_bt" type="button" value="會員資訊" onclick = "location.href = 'member_info_page.php'">
-            <?php
-            }
-            else
-            {
+                <?php
+                }
+                else
+                {
                 ?>
                 <input class = "bt" id = "mem_info_bt" type="button" value="會員資訊" onclick = "location.href = 'member_info_page.php'">
                 <input class = "bt" id = "logout_bt" type="button" value="登出" onclick = "location.href = 'log_out.php'">
                 <input class = "bt" id = "change_bt" type="button" value="修改密碼" onclick = "location.href = 'change_password.php'">
-            <?php
-            }
-            ?>
+                <?php
+                }
+                ?>
             </div>
             <div class = "sections" id = "section_1">
                 <img class = "pic" id = "pic_a" src = "./pics/0426.png"/>
@@ -52,17 +52,17 @@ else
                         <p class = "text_a" id = "cat_text">A:時效:半年 介紹:抓資料庫的 價格:4799</p>
                         <p class = "text_a" id = "cat_text">B:時效:一年 介紹:抓資料庫的 價格:8999</p>
                         <p class = "text_a" id = "cat_text">C:時效:兩年 介紹:抓資料庫的 價格:12999</p>
-                        <input id = "reserve_bt" type="button" value="課程預約" onclick = "location.href = 'reserve_page.php'">
+                        <input class = "bt_2" id = "reserve_bt" type="button" value="課程預約" onclick = "location.href = 'reserve_page.php'">
                     </div>
                 </div>
             </div>
             <div class = "sections" id = "section_2">
-                <img class = "pic" id = "pic_b" src = "./pics/0426.png"/>
+                <img class = "pic" id = "pic_b" src = "./pics/unnatural.png"/>
                 <div class = "base" id = "base_2">
-                <div class = "adjust_index">
-                <p class = "text_b" id = "dog">汪汪隊出任務</p>
-                    <p class = "text_b" id = "dog_text">幾張教練照片</p>
-                    <input id = "reserve_bt" type="button" value="教練簡介" onclick = "location.href = 'coach_page.php'">
+                    <div class = "adjust_index">
+                        <p class = "text_b" id = "dog">汪汪隊出任務</p>
+                        <p class = "text_b" id = "dog_text">幾張教練照片</p>
+                        <input class = "bt_2" id = "reserve_bt" type="button" value="教練簡介" onclick = "location.href = 'coach_page.php'">
                     </div>
                 </div>
             </div>
