6a914113eef09fef83be963b9542c70e2331a842
AGENT-FONG <ericwf36@gmail.com>
2023-05-04 20:14:43 +0800

    Update manager_page.php

--- manager_page.php
+++ manager_page.php
@@ -22,7 +22,6 @@ else
     <body>
         <div id = "background">
             <div id = "banner">
-                <input id = "index_bt" style="font-size:34px;float:center;cursor:pointer"type="button" value="健身房" onclick = "location.href = 'index.php'">
                 <?php
             if ($log_check == 0)
             {
