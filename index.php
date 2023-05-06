eb3d4c1cb0e3be9debb04f07c7b0a13720a7fda0
mark90645 <132208738+mark90645@users.noreply.github.com>
2023-05-06 16:53:01 +0800

    0506

--- index.php
+++ index.php
@@ -78,9 +78,8 @@ else
         </div>
         <div class = "end">
             <p>聯絡資訊</p>
-            
             <input class = "contact_pic" id = "pic_c" type="button" onclick = "location.href = 'reserve_page.php'"><!--以下大改-->
-            <input class = "contact_pic" id = "pic_d" type="button" onclick = "location.href = 'reserve_page.php'"><img src = "./pics/instagram.png"></button>
+            <input class = "contact_pic" id = "pic_d" type="button" onclick = "location.href = 'reserve_page.php'"><img src = "./pics/instagram.png" id = "ig"></button>
         </div>
     </body>
 </html>
