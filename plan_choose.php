923d8e53cdeb35b28eb97f7988e54342de0b0a12
AGENT-FONG <ericwf36@gmail.com>
2023-05-08 22:29:16 +0800

    Create plan_choose.php

---
+++ plan_choose.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_COOKIE["member_account"]))
+{
+    $log_check = True;
+    $conn=require_once "configure.php";
+    $cookie = $_COOKIE['member_account'];
+}
+else
+{
+    $log_check = False;
+}
+?>
