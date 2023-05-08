4c83ebfef803973b12920e498e3ee974e2099fe0
AGENT-FONG <ericwf36@gmail.com>
2023-05-09 01:30:02 +0800

    方案更新01:29
    把功能弄完整

--- plan_choose.php
+++ plan_choose.php
@@ -31,31 +31,47 @@ else
         </h1>
         <div>
             <h3 style="color:#e620a4"><?php echo $member_name; ?>，您好，請選擇以下健身方案</h3>
+            <?php
+                $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+                $sql = "SELECT * FROM plan_choose WHERE member_name = '$member_name'";
+                $result = mysqli_query($link, $sql);
+                if(mysqli_num_rows($result) == 0){
+                    echo "<h3 style='color:blue;'>您現在沒有選擇任何方案！</h3>";
+                }else{
+                    $row = mysqli_fetch_assoc($result);
+                    $plan = $row["plan_id"];
+                    echo "<h3 style='color:blue;'>您現在選擇的方案為「".$plan."」</h3>";
+                }
+                mysqli_close($link);           
+            ?>
         </div>
         <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
         <p class = "input_bar">
             預計選擇方案：
-            <label><input type="radio" name="plan" value="新手方案" checked>新手方案</label>
+            <label><input type="radio" name="plan" value="新手方案">新手方案</label>
             <label><input type="radio" name="plan" value="進階方案">進階方案</label>
             <label><input type="radio" name="plan" value="選手級達人">選手級達人</label></p>
             <input id = "log_in_bt" type="submit" value="確認方案" name = "submit">
             <input id = "log_in_bt" type="submit" value="清空我的方案" name = "clear">
+            <br/><br/>
+            <input class = "bt" id = "logout_bt" type="button" value="提交方案" onclick="Updating();">
         </form>
+        <script>
+            function Updating(){
+                var message = "方案資料更新中，請稍後！";
+                alert(message);
+                location.href = 'plan_choose.php';
+            }
+        </script>
         <!-- 選擇方案 -->
         <?php
             if(isset($_POST['submit'])){
-                $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+                if(isset($_POST['plan'])){
+                    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
                 if(!$link){
                     die('資料庫連線失敗！'.mysqli_connect_error());
                 }
                 $plan = $_POST['plan'];
-                $sql_check = "SELECT * FROM plan_choose WHERE member_name = '$member_name'";
-                $result_check = mysqli_query($link, $sql_check);
-                if(mysqli_num_rows($result_check) == 0){
-                    echo "<h3 style='color:blue;'>您現在沒有選擇任何方案！</h3>";
-                }else{
-                    echo "<h3 style='color:blue;'>您現在選擇的方案為「".$plan."」</h3>";
-                }
                 $sql1 = "SELECT member_id, member_name, member_account FROM regular_member WHERE member_name = '$member_name'";
                 $result1 = mysqli_query($link, $sql1);
                 $row = mysqli_fetch_assoc($result1);
@@ -66,13 +82,15 @@ else
                 $result2 = mysqli_query($link, $sql2);
                 if(mysqli_num_rows($result2) == 0){
                     $sql3="INSERT INTO plan_choose(member_id, member_name, member_account, plan_id) VALUES('$id','$name','$account','$plan')";
-                    $result3 = mysqli_query($link, $sql3);
+                    $result3 = mysqli_query($link, $sql3);                
                 }else{
                     $sql4 = "UPDATE plan_choose SET plan_id = '$plan' WHERE member_id = '$id'";
                     $result4 = mysqli_query($link, $sql4);
                 }
                 
                 mysqli_close($link);
+                }else{echo '<script>alert("未選擇方案！");</script>';}
+                
             }
             if(isset($_POST['clear'])){
                 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
