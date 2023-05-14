5606809f8e5446ae6137c4398b8a33c7b9b87ac7
AGENT-FONG <ericwf36@gmail.com>
2023-05-14 23:46:33 +0800

    修改密碼更新

--- change_password.php
+++ change_password.php
@@ -1,3 +1,14 @@
+<?php
+    $conn=require_once("configure.php");
+    session_start();
+    $cookie = $_COOKIE['member_account'];
+    $sql = "SELECT phone FROM regular_member WHERE member_account = '".$cookie."'";
+    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    $result = mysqli_query($link,$sql);
+    $row = mysqli_fetch_assoc($result);
+    $phone = $row['phone'];
+    mysqli_close($conn);
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -30,7 +41,9 @@
                 <div id = "the_back_4">
                     <form name="changeForm" method="post" action="change_password_procedure.php" onsubmit="return validateCheck()">
                         <p class = "input_bar">
-                            輸入帳號：<input type="text" name="member_account"></p>
+                            帳號名稱：<?php echo $cookie;?></p>
+                        <p class = "input_bar">
+                            手機號碼：<?php echo $phone;?></p>
                         <p class = "input_bar">
                             輸入舊密碼：<input type="password" name="old_password"></p>
                         <p class = "input_bar">
