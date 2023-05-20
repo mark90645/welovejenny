ef05e1eccb39abcbee14df558786397fbc3f2e18
AGENT-FONG <ericwf36@gmail.com>
2023-05-20 18:56:40 +0800

    Update configure.php

--- configure.php
+++ configure.php
@@ -4,7 +4,7 @@ define('DB_SERVER', '25.41.90.151:3306');
 define('DB_USERNAME', 'share');
 define('DB_PASSWORD', 'ihaveabigdick');
 define('DB_NAME', 'gym');
- 
+    
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // 輸入中文也OK的編碼
