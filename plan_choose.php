39c6fa4ff6f545cad81c898dbdc98505d012935b
mark90645 <132208738+mark90645@users.noreply.github.com>
2023-05-22 01:35:06 +0800

    0521-成發好累事情都沒做

--- plan_choose.php
+++ plan_choose.php
@@ -30,19 +30,21 @@ else
             </div>
             <div class = "side_block">
                 <h3 class = "side_text"><?php echo $member_name; ?>，您好<br>請選擇健身方案</h3>
+                <div class = "line" id = "line_d"></div>
                 <?php
                     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
                     $sql = "SELECT * FROM plan_choose WHERE member_name = '$member_name'";
                     $result = mysqli_query($link, $sql);
                     if(mysqli_num_rows($result) == 0){
-                        echo "<h3 class = 'side_text'>您沒有選擇任何方案！</h3>";
+                        echo "<h3 class = 'side_text' id = 'side_text2'>您沒有選擇任何方案！</h3>";
                     }else{
                         $row = mysqli_fetch_assoc($result);
                         $plan = $row["plan_id"];
-                        echo "<h3 class = 'side_text'>您現在選擇的方案為<br>「".$plan."」</h3>";
+                        echo "<h3 class = 'side_text' id = 'side_text2'>您現在選擇的方案為<br>「".$plan."」</h3>";
                     }
                     mysqli_close($link);           
                 ?>
+                <div class = "line" id = "line_e"></div>
             </div>
             <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
                 <p class = "input_bar">
