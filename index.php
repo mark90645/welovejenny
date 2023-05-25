514040456f82530926657a4fbb28cff8352f2d85
mickey910611 <132209186+mickey910611@users.noreply.github.com>
2023-05-25 12:02:55 +0800

    .

--- index.php
+++ index.php
@@ -43,27 +43,79 @@ else
                 }
                 ?>
             </div>
-            <div class="color-lump">
-            <section>
-            <div class="container">
-             <span id="previous">＜</span>
-             <span id="next">＞</span>
-             <div id="slider" class="slider">
-             <img src="https://i.pinimg.com/564x/c2/6a/87/c26a87129b59737f8c3435091810816a.jpg"  />
-             <img src="https://i.pinimg.com/564x/d1/d0/05/d1d0058fc64a07acba52c9bbfd2da7b5.jpg  "  />
-             <img src="https://i.pinimg.com/564x/a0/e5/f6/a0e5f6bc70cf643be31be521f560885e.jpg" />
-             <img src="https://i.pinimg.com/564x/d1/12/46/d112468ed2e1a0d8f5e0c60b6a1af042.jpg" />
-             <img src="https://i.pinimg.com/564x/e8/38/22/e838229f1592b1552866c4f1857dbee2.jpg" />
-             </div>
 
-             <ul id="dots" class="dots">
-              <li></li>
-              <li></li>
+            <div class="color-lump">          
+            <div class="slideshow-container">
+              <div class="mySlides fade">
+              <img src="https://i.pinimg.com/564x/c2/6a/87/c26a87129b59737f8c3435091810816a.jpg" style="width:100%">
+              </div>
 
-              </ul>
+              <div class="mySlides fade">
+              <img src="https://i.pinimg.com/564x/d1/d0/05/d1d0058fc64a07acba52c9bbfd2da7b5.jpg" style="width:100%">
+              </div>
+
+              <div class="mySlides fade">
+              <img src="https://i.pinimg.com/564x/a0/e5/f6/a0e5f6bc70cf643be31be521f560885e.jpg" style="width:100%">
+              </div>
+
+              <div class="mySlides fade">
+              <img src="https://i.pinimg.com/564x/d1/12/46/d112468ed2e1a0d8f5e0c60b6a1af042.jpg" style="width:100%">
+              </div>
+
+              <div class="mySlides fade">
+              <img src="https://i.pinimg.com/564x/e8/38/22/e838229f1592b1552866c4f1857dbee2.jpg" style="width:100%">
+              </div>
+
+              <a class="prev" onclick="plusSlides(-1)">❮</a>
+              <a class="next" onclick="plusSlides(1)">❯</a>
+
+              </div>
+       
+
+            <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span> 
+            <span class="dot" onclick="currentSlide(2)"></span> 
+            <span class="dot" onclick="currentSlide(3)"></span> 
+            <span class="dot" onclick="currentSlide(4)"></span> 
+            <span class="dot" onclick="currentSlide(5)"></span> 
             </div>
-            </section>                 
+            <script>
+              var slideIndex = 0;
+                showSlides();
+                function showSlides() {
+                    var i;
+                    var slides = document.getElementsByClassName("mySlides");
+                    var dots = document.getElementsByClassName("dot");
+                    for (i = 0; i < slides.length; i++) {
+                      slides[i].style.display = "none";  
+                    }
+                    
+                    if (slideIndex > slides.length-1) {slideIndex = 0}    
+                    if (slideIndex < 0) {slideIndex = slides.length-1} 
+                    for (i = 0; i < dots.length; i++) {
+                        dots[i].className = dots[i].className.replace(" active", "");
+                    }
+                    slides[slideIndex].style.display = "block";  
+                    dots[slideIndex].className += " active";
+                    timeout=setTimeout(showSlides, 4000);
+                    slideIndex++;
+                    return timeout;
+                  }
+
+                  function plusSlides(n) {
+                      clearTimeout(timeout);
+                      slideIndex += n-1;
+                      showSlides();
+                    }
+                    
+                    function currentSlide(n) {
+                      clearTimeout(timeout);
+                      slideIndex = n;
+                      showSlides();
+                    }
+            </script>             
             </div>
+
 
             <div class="color-lump-2"> <div class = "adjust_index">
                         <p class = "text_a" id = "cat_text"><b>方案內容介紹</b></p> 
@@ -112,60 +164,6 @@ else
             <input class = "contact_pic" id = "pic_d" type="button" onclick = "location.href = 'reserve_page.php'"><img src = "./pics/instagram.png" id = "ig"></button>
         </div>
 
-        <script>
-    const nextEl = document.getElementById("next");
-  const previousEl = document.getElementById("previous");
-  const sliderEl = document.getElementById("slider");
-  const dots = document.getElementById("dots");
-  const imgCounts = sliderEl.children.length;
-  nextEl.addEventListener("click", () => (slideProxy.index += 1));
-  previousEl.addEventListener("click", () => (slideProxy.index -= 1));
-  setClickEventToDots();
-  window.onresize = debounce(calculateWidth);
-  const slideProps = { index: 0 };
-  const slideHandler = {
-    set(obj, prop, value) {
-      if (prop === "index") {
-        if (value < 0 || value >= imgCounts) return;
-        setDotToInactive();
-        obj[prop] = value;
-        calculateWidth();
-        setActiveDot();
-      }
-    },
-  };
-  const slideProxy = new Proxy(slideProps, slideHandler);
-  setActiveDot();
-  function calculateWidth() {
-    const imgWidth = sliderEl.offsetWidth;
-    const recomputedWidth = slideProps.index * imgWidth;
-    sliderEl.scrollLeft = recomputedWidth;
-  }
-  function setDotToInactive() {
-    const { index } = slideProps
-    dots.children[index].classList.remove('dot--active')
-  }
-  function setActiveDot() {
-    const { index } = slideProps
-    dots.children[index].classList.add('dot--active')
-  }
-  function setClickEventToDots() {
-    for (let i = 0; i < dots.children.length; i++) {
-      const li = dots.children[i]
-      li.addEventListener('click', () => {
-        slideProxy.index = i
-      })
-    }
-  }
-  function debounce(func, timeout = 100) {
-    let timer;
-    return (...args) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        func.apply(this, args);
-      }, timeout);
-    };
-  }
-    </script>
+
     </body>
 </html>
