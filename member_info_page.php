4b757539283f08e330250b2330ec95c23594ab9c
mark90645 <132208738+mark90645@users.noreply.github.com>
2023-06-09 02:33:25 +0800

    0609個人頁面好好看

--- member_info_page.php
+++ member_info_page.php
@@ -23,14 +23,15 @@ else
     <head>
         <meta charset = "UTF-8"></meta>
         <title>會員資訊</title>
-        <link rel = "stylesheet" href = "./CSS/board.css" />
+        <!--<link rel = "stylesheet" href = "./CSS/board.css" />-->
         <link rel = "stylesheet" href = "./CSS/member_info_page.css" />
     </head>
     <body>
-        <div class = "background">
-            <div class = "banner">             
-                <input id = "index_bt" type="button" value="回到首頁" onclick = "location.href = 'index.php'">
-            </div>
+        <div class = "background">         
+            <input id = "index_bt" type="button" value="回到首頁" onclick = "location.href = 'index.php'">
+            <input class = "bt" id = "back_bt1" type="button" value="預約課程" onclick = "location.href = 'reserve_page.php'">
+            <input class = "bt" id = "back_bt2" type="button" value="方案選擇" onclick = "location.href = 'plan_choose.php'">
+            <input class = "bt" id = "change_bt" type="button" value="修改個人資料" onclick = "location.href = 'change_password.php'">
             <?php
             $member_id = $member_id;
             // 大頭照上傳功能
@@ -114,28 +115,18 @@ else
 
             <div id="section_1">
                 <img style="width: 200px" alt="memberpic" id="head_pic" src="<?php echo $headPicPath; ?>">
-                <h2>歡迎，<?php echo $member_name; ?></h2>
+                <div class = "seperate_line"></div>
+                <h2 class = "info_text text1">歡迎，<?php echo $member_name; ?></h2>
                 <form method="POST" action="" enctype="multipart/form-data">
-                    <input type="file" name="photo" accept="image/jpeg, image/png" required>
-                    <button type="submit">更換圖片</button>
+                    <input  class = "pic_bt pb1" type="file" name="photo" accept="image/jpeg, image/png" required>
+                    <button class = "pic_bt pb2" type="submit">更換圖片</button>
                 </form>
                 <form method="POST" action="">
                     <input type="hidden" name="clear" value="1">
-                    <button type="submit">清空圖片</button>
+                    <button class = "pic_bt pb3" type="submit">清空圖片</button>
                 </form>
             </div>
-
-
-
-
-
-            <input class = "bt" id = "back_bt1" type="button" value="預約課程" onclick = "location.href = 'reserve_page.php'">
-            <br>
-            <br>
-            <input class = "bt" id = "back_bt2" type="button" value="方案選擇" onclick = "location.href = 'plan_choose.php'">
-            <br>
-            <br>
-             <div id = "section_2">     
+            <div id = "section_2">     
             <?php
                 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
                 $sql = "SELECT * FROM plan_choose WHERE member_name = '$member_name'";
@@ -146,26 +137,29 @@ else
                 $new=strtotime("+6 Months");
                 $advance=strtotime("+1 Years");
                 $master=strtotime("+2 Years");
-                echo "<h3>現在時間：".$DateAndTime."</h3>";
+                echo "<h3 class = 'info_text text2'>現在時間：".$DateAndTime."</h3>";
                 if(mysqli_num_rows($result) == 0){
-                    echo "<h3 style='color:blue;'>您現在沒有選擇任何方案！</h3>";
+                    echo "<h3 class = 'info_text text3'>您現在沒有選擇任何方案！</h3>";
                 }else{
                     $row = mysqli_fetch_assoc($result);
                     $plan = $row["plan_id"];
                     if($plan=="新手方案"){
-                    echo "<h3 style='color:blue;'>目前已選「".$plan."」，時限到".date('Y-m-d', $new)."</h3>";
+                    echo "<h3 class = 'info_text text3'>目前已選「".$plan."」，時限到".date('Y-m-d', $new)."</h3>";
                     }
                     if($plan=="進階方案"){
-                    echo "<h3 style='color:blue;'>目前已選「".$plan."」，時限到".date('Y-m-d', $advance)."</h3>";
+                    echo "<h3 class = 'info_text text3'>目前已選「".$plan."」，時限到".date('Y-m-d', $advance)."</h3>";
                     }
                     if($plan=="達人方案"){
-                    echo "<h3 style='color:blue;'>目前已選「".$plan."」，時限到".date('Y-m-d', $master)."</h3>";
+                    echo "<h3 class = 'info_text text3'>目前已選「".$plan."」，時限到".date('Y-m-d', $master)."</h3>";
                     }
                 }
                 mysqli_close($link);           
             ?>
             </div>
-                <div id = "section_3"> 
+                <div id = "section_3">
+                <h3 class = "class_text">課 程</h3>
+                <div class = "class_line"></div>
+                <div class = "text_position">
                     <?php
                         $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
                         $sql = "SELECT booking_date, class_type
@@ -177,22 +171,21 @@ else
                                 $bookdate = $row['booking_date'];
                                 $type = $row['class_type'];
                                 if ($type == "yoga") {
-                                    echo "<h3 style='color:blue;'>已為您預約".$bookdate."的瑜珈課程</h3>";
+                                    echo "<h3 class = 'booking_text'>已為您預約".$bookdate."的瑜珈課程</h3>";
                                 } elseif ($type == "bike") {
-                                    echo "<h3 style='color:blue;'>已為您預約".$bookdate."的飛輪課程</h3>";
+                                    echo "<h3 class = 'booking_text'>已為您預約".$bookdate."的飛輪課程</h3>";
                                 } elseif ($type == "aerobic") {
-                                    echo "<h3 style='color:blue;'>已為您預約".$bookdate."的有氧舞蹈課程</h3>";
+                                    echo "<h3 class = 'booking_text'>已為您預約".$bookdate."的有氧舞蹈課程</h3>";
                                 }else{
-                                    echo "<h3 style='color:blue;'>您現在沒有預約任何課程！</h3>";
+                                    echo "<h3 class = 'booking_text'>您現在沒有預約任何課程！</h3>";
                                 }
                             }
                             
                         mysqli_close($link);           
                     ?>
-                <br/><br/><br/>
+                </div>
                 </div>
             </div>
-        </div>
         </div>
     </body>
 </html>
